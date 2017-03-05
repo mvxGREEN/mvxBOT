6a566da3e417b6b113d59fe686fcedb74c579a9e
sw8lde <sw8lde@gmail.com>
2017-03-05 15:15:12 -0600

    Updated test

---
+++ test.js
@@ -0,0 +1,57 @@
+var at = "https://api.groupme.com/v3"
+var HTTPS = require('https');
+
+var botID = process.env.BOT_ID;
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^hi$/i;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "Hi";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+exports.respond = respond;
