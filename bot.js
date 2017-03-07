f16829d8fc52c6d8a06d1e17760c17deada56fbc
sw8lde <sw8lde@gmail.com>
2017-03-06 23:33:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var mention = require('./mention.js');
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /(^|\s)CSSA Bot(\s|$)/i,
-      mentionRegex = /(^|\s)@all(\s|$)/i;
+      mentionRegex = /(^|\s)all(\s|$)/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
